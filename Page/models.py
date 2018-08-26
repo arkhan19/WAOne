aed746ee927e9f22441b4b22be4d4c6692908da8
f3n1xx <arkspeed@gmail.com>
2018-08-27 05:05:55 +0530

    Page Model

--- Page/models.py
+++ Page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Post.models import Post
+
 
 # Create your models here.
+class Page(models.Model):
+    name = models.CharField(unique=True, max_length=42)
+    email = models.EmailField(max_length=75)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- Post/models.py
+++ Post/models.py
@@ -8,7 +8,7 @@ class Post (models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, max_length=255)
     content = models.TextField()
-    created_on=models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     author = models.TextField()
 
 
